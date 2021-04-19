5d7355061f9a5944dca34863631da36f1b2fbded
shyamghan17 <48532181+shyamghan17@users.noreply.github.com>
2021-04-19 19:36:53 +0545

    .

--- WeakMap.js
+++ WeakMap.js
@@ -1,5 +1,12 @@
 const weakMap = new WeakMap()
 console.log(weakMap)
 let obj = {}
-weakMap.set(obj, 'hello')
-console.log(weakMap)
+// weakMap.set(obj, 'hello')
+// console.log(weakMap)
+// console.log(weakMap.get(obj))
+// console.log(weakMap.has(obj))
+// console.log(weakMap.delete(obj))
+
+for (let i of weakMap) {
+    console.log(i)
+}
