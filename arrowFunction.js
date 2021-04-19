53e2e180d097f919dd1c350bafe870d2790b248c
shyamghan17 <48532181+shyamghan17@users.noreply.github.com>
2021-04-19 16:31:53 +0545

    Update arrowFunction.js

---
+++ arrowFunction.js
@@ -0,0 +1,18 @@
+
+
+
+// Example 1
+// let greet = () => console.log ('hello')
+// greet ()
+
+
+//Example 2
+// let greet = x => console.log (x)
+// greet('hello')
+
+// Example 3
+
+let age =39
+
+let welcome = (age < 18) ? () => console.log('Baby') : () => console.log('Adult')
+welcome()
