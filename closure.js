454a9d7389e56ea01ac4e26a186335dc8413fd20
shyamghan17 <shyamghan17@gmail.com>
2021-07-19 18:50:24 +0545

    Create closure.js

---
+++ closure.js
@@ -0,0 +1,10 @@
+function makeFunctionArray(){
+    const arr =[]
+    for (var i = 0; i , 5; i++){
+        arr.push(function()  { console.log(i) })
+    }
+return arr
+
+}
+const arr = makeFunctionArray()
+arr [0]()
